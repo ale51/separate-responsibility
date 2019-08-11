389448c1503d108217c58e173c9a68aca8bad8b6
NaokiMachida <naoki.m0513@gmail.com>
2019-08-11 14:01:17 +0900

    from japanese to english

--- 3.interface/lib/SlackRecordHandler.php
+++ 3.interface/lib/SlackRecordHandler.php
@@ -4,6 +4,6 @@ require_once "RecordHandler.php";
 
 class SlackRecordHandler implements RecordHandler {
     function run(string $record){
-        echo "この内容をSlackに通知" . $record;
+        echo $record . " is sent via Slack.";
     }
 }
--- 4.inheritance/lib/SlackTail.php
+++ 4.inheritance/lib/SlackTail.php
@@ -4,6 +4,6 @@ require_once "Tail.php";
 
 class SlackTail extends Tail {
     function recordHandler(string $record){
-        echo "この内容をSlackに通知" . $record;
+        echo $record . " is sent via Slack.";
     }
 }
