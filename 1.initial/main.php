c8e6ed132f35c230cffcdaee685ee9da0d426c33
NaokiMachida <naoki.m0513@gmail.com>
2019-08-11 13:38:20 +0900

    delete comment & unnecessary if block.

--- 1.initial/main.php
+++ 1.initial/main.php
@@ -1,36 +1,25 @@
 <?php
-//require_once "vendor/autoload.php";
 
 $sleepInterval = 1;
 $filePath = "./test.log";
 
-//    anonymous-function
-
 $fp = fopen($filePath, 'r');
 
-// 一番末尾のポインタの位置を取得
 $position = getLastPosition($fp);
 
 do{
 
-    // 一番末尾のポインタの位置を取得
     $nextPosition = getLastPosition($fp);
 
-    // ポインタが増えている場合
     if($nextPosition > $position){
 
-        // 現在のポインタをセット
         fseek($fp, $position);
 
-        // 現在のポインタから増加分のデータを取得
         $record = fread($fp, $nextPosition - $position);
 
-        if(preg_match("/^ERROR/", $record)) {
-            echo $record;
-        }
+        echo $record;
     }
 
-    // ポインタの更新
     $position = $nextPosition;
 
     sleep($sleepInterval);
--- 3.interface/lib/Tail.php
+++ 3.interface/lib/Tail.php
@@ -19,29 +19,21 @@ class Tail
     {
         $fp = fopen($this->filePath, 'r');
 
-        // 一番末尾のポインタの位置を取得
         $position = $this->getLastPosition($fp);
 
         do {
 
-            // 一番末尾のポインタの位置を取得
             $nextPosition = $this->getLastPosition($fp);
 
-            // ポインタが増えている場合
             if ($nextPosition > $position) {
 
-                // 現在のポインタをセット
                 fseek($fp, $position);
 
-                // 現在のポインタから増加分のデータを取得
                 $record = fread($fp, $nextPosition - $position);
 
-                if (preg_match("/^ERROR/", $record)) {
-                    $recordHandler->run($record);
-                }
+                $recordHandler->run($record);
             }
 
-            // ポインタの更新
             $position = $nextPosition;
 
             sleep(self::SLEEP_INTERVAL);
